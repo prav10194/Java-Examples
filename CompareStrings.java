7c20f70f2f92277e972e25b5ebdfa971d89b8278
prav10194 <prav10194@gmail.com>
2017-01-31 01:08:56 -0600

    Create CompareStrings.java
    Similar question - Assume you have a method isSubstring which checks if one word is a substring of
    another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using
    only one call to isSubstring (i.e., “waterbottle” is a rotation of “erbottlewat”)

---
+++ CompareStrings.java
@@ -0,0 +1,34 @@
+class CompareStrings
+{
+  public static void main(String[] args)
+  {
+    String compareString1="hello";
+    String compareString2="elloh";
+		char[] charCompareString2=compareString2.toCharArray();
+		
+		for(int i=0;i<compareString2.length();i++)
+		{
+			char[] w=new char[compareString2.length()];
+			int j2=0;
+			
+      //adding characters from backwords and put it in front
+      for(int j1=compareString2.length()-i-1;j1<=compareString2.length()-1;j1++)
+			{
+				w[j2++]=charCompareString2[j1];
+			}
+      
+      //adding the rest of the string
+			for(int j1=0;j1<compareString2.length()-i-1;j1++)
+			{
+				w[j2++]=charCompareString2[j1];
+			}
+      
+      //compare each iteration with original string
+			if(Arrays.equals(compareString1.toCharArray(),w))
+			{
+			  System.out.println("\n"+String.valueOf(i)+" ");
+			  System.out.println(w);
+			}
+		}
+  }
+}
