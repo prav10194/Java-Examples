4cedeb5eb36202a3f54d97b94fcf878aa630334e
Pranav Bhatia <prav10194@gmail.com>
2017-04-17 13:31:28 -0500

    Update TotalPathsInMaze.java

--- TotalPathsInMaze.java
+++ TotalPathsInMaze.java
@@ -13,6 +13,22 @@ public class TotalPathsInMaze
 		return mat[r + 1][c] + mat[r][c + 1];
 		}
 	
+	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+	    int width = obstacleGrid[0].length;
+	    int[] dp = new int[width];
+	    dp[0] = 1;
+	    for (int[] row : obstacleGrid) {
+	        for (int j = 0; j < width; j++) {
+	            if (row[j] == 1)
+	                dp[j] = 0;
+	            else if (j > 0)
+	                dp[j] += dp[j - 1];
+	        }
+	    }
+	    return dp[width - 1];
+	}
+	
+	
 	public static void main(String args[])
 	{
 		int m=5; //total rows
@@ -23,8 +39,11 @@ public class TotalPathsInMaze
 				mat[i][j] = -1;
 				}
 			}
+		int[][] mat1={{0,0,0},{0,1,0},{0,0,0}}; //matrice with obstacles - 0 means no wall, 1 means a wall
 		
 		System.out.println("Number of Unique Paths - "+backtrack(0, 0, m, n, mat));
+		System.out.println("Number of Unique Paths - "+uniquePathsWithObstacles(mat1));
+	
 
 		
 	}
